2fc395e51e12f95abd2a00d0914e9263a2fa1952
boldoo-code <jboldoo98@gmail.com>
2025-09-10 12:10:00 +0800

    lab2.4

--- store_app/views.py
+++ store_app/views.py
@@ -1,28 +1,37 @@
 from django.shortcuts import render
 
+
 def index(request):
     return render(request, "index.html")
+
 
 def cart(request):
     return render(request, "cart.html")
 
+
 def dashboard(request):
     return render(request, "dashboard.html")
+
 
 def order_complete(request):
     return render(request, "order_complete.html")
 
-def product_detail(request):
-    return render(request, "product-detail.html")
+
+def product_detail(request, id):
+    return render(request, "product-detail.html", {'id': id})
+
 
 def register(request):
     return render(request, "register.html")
 
+
 def search_result(request):
     return render(request, "search-result.html")
 
+
 def signin(request):
     return render(request, "signin.html")
+
 
 def store(request):
     return render(request, "store.html")
