d07c9b0059cce3eea9ee128a1d2cab1147a4135b
boldoo-code <jboldoo98@gmail.com>
2025-11-04 15:32:01 +0800

    lab9 duussan

--- store_app/unit.py
+++ store_app/unit.py
@@ -9,20 +9,21 @@ def category_data(request):
     return {'categories': categories}
 
 
+# def menu_links(request):
+#     links = Category.objects.all()
+#     return dict(links=links)
+
 def counter(request):
     cart_count = 0
     # if 'admin' in request.path:
     #     return {}
-    cart = Cart.objects.get(cart_id=_cart_id(request))
-    cart_items = CartItem.objects.filter(cart=cart)
-
-    print(f'##############${cart_items}')
-    for item in cart_items:
-        cart_count += item.quantity
-    # try:
-    # except Cart.DoesNotExist:
-    #     cart_count = 0
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart)
+        for item in cart_items:
+            cart_count += item.quantity
+    except Cart.DoesNotExist:
+        cart_count = 0
     return dict(cart_count=cart_count)
-
 
 # cart 0 doosh hasagdaj bolohgui bas baraanii uldegdel 5 baival 5aas deesh yvj bolohgui
