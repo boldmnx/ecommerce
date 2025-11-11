cd29ba3e790181f2e2b2f66927aa028bbd59d29e
boldoo-code <jboldoo98@gmail.com>
2025-11-11 17:01:57 +0800

    lab10 search

--- store_app/views.py
+++ store_app/views.py
@@ -1,6 +1,9 @@
+from django.shortcuts import render
+from .models import Product
 from django.shortcuts import get_object_or_404, redirect, render
 from .models import Product, Category
 from django.core.paginator import Paginator
+from django.db.models import Q
 
 
 def index(request):
@@ -31,7 +34,47 @@ def register(request):
 
 
 def search_result(request):
-    return render(request, "search-result.html")
+    keyword = request.GET.get('keyword', '').strip()
+    min_price = request.GET.get('min')
+    max_price = request.GET.get('max')
+    cat_slug = request.GET.get('category', '')
+    products = Product.objects.all()
+
+    # if cat_slug and min_price and max_price:
+    #     categories = get_object_or_404(Category, slug=cat_slug)
+    #     products = Product.objects.filter(
+    #         category=categories, price__gte=min_price, price__lte=max_price)
+
+    #     products = Product.objects.filter(Q(category=categories) & Q(
+    #         price__gte=min_price) & Q(price__lte=max_price))
+
+    # Хайлтын keyword
+    if keyword:
+        products = products.filter(
+            Q(product_name__icontains=keyword) |
+            Q(description__icontains=keyword)
+        )
+
+    if cat_slug:
+        categories = get_object_or_404(Category, slug=cat_slug)
+        products = Product.objects.filter(category=categories)
+
+    # Үнийн шүүлтүүр
+    if min_price:
+        products = products.filter(price__gte=min_price)
+
+    if max_price:
+        products = products.filter(price__lte=max_price)
+
+    context = {
+        "products": products,
+        "keyword": keyword,
+        "min_price": min_price,
+        "count": len(products),
+        "keyword": keyword,
+        "max_price": max_price
+    }
+    return render(request, "store.html", context)
 
 
 def signin(request):
@@ -49,14 +92,14 @@ def store(request, slug=None):
     if slug != None:
         categories = get_object_or_404(Category, slug=slug)
         products = Product.objects.filter(category=categories)
-        p = Paginator(products, 3)
+        p = Paginator(products, 6)
         page = request.GET.get('page')
         page_products = p.get_page(page)
         count = products.count()
         garchig = categories
     else:
         products = Product.objects.all().filter(is_available=True)
-        p = Paginator(products, 3)
+        p = Paginator(products, 6)
         page = request.GET.get('page')
         page_products = p.get_page(page)
         count = products.count()
